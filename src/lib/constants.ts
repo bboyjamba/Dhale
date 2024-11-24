158d6d5cb4ba1543446dbe9be41c29a2608d7ad1
bboyjamba <36270989+bboyjamba@users.noreply.github.com>
2024-11-24 15:15:25 -0700

    corrected social srcs still looking for img source.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,9 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+  github: "https://github.com/bboyjamba",
+  linkedin: "https://www.linkedin.com/in/damienhale/",
+  medium: "https://medium.com/",
+  discord: "https://discordapp.com/users/dk.master",
+  dribble: "https://dribbble.com/",
 };
 
 export const loaderAnimation = [
